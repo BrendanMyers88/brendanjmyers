cd02f7241539788d0d21c3da23f4ea429b789823
BrendanMyers88 <myersb88@gmail.com>
2019-01-10 16:59:34 -0800

    Adds 'Tools' and 'Clients' section. Tools is filled out correctly, and next is to add clients properly. Footer situation needs to be addressed as well so contact information is explicit.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './HomePage.css';
-import {Sticky, Segment} from 'semantic-ui-react';
-import {animateScroll as scroll, scrollSpy, scroller} from 'react-scroll';
+import {Sticky, Segment, Image, Header} from 'semantic-ui-react';
+import {animateScroll as scroll} from 'react-scroll';
 import {NavMenu} from '../NavMenu'
 
 
@@ -39,7 +39,63 @@ export default class HomePage extends Component {
                     </Sticky>
                 </div>
 
-                <Segment className="about">asfsafasfasdfasdf</Segment>
+                <div>
+                    <Image className="self-photo" src={require('./images/BrendanPhoto.jpg')} bordered/>
+                    <Segment basic compact className="about">
+                        <div className="about-header">About</div>
+                        <div className="about-body">
+                            <div>
+                                <em>
+                                    Hey there! I'm a full stack developer that got started later than most. I got my
+                                    Bachelor's Degree from the University of Idaho in 2012 in Recreation. After a few
+                                    years of working in apartment maintenance, park management, and landscaping followed
+                                    with a a two-month long visit to Thailand in between, I decided to change career
+                                    paths to programming in 2018.
+                                </em>
+                            </div>
+                            <br/><br/>
+                            <div>
+                                Over the past year, I've learned a lot about programming. Like most programmers starting
+                                out, I didn't know what I didn't know. I ran headlong for Python as I'd heard it was a
+                                great starting place for beginners. That Python base would eventually lead to a love
+                                of web development through the Django web framework. I still use Python as my primary
+                                back-end language, but front-end
+                                development was where I found my biggest strengths. It allowed me to blend my eye for
+                                design with practical, real world solutions.
+                            </div>
+                            <br/><br/>
+                        </div>
+                    </Segment>
+                    <br/>
+                    <Segment basic compact className="experience">
+                        <div className="tools">
+                            <div className="primary">Tools I've Used</div>
+                            <div className="tools-container">
+                                <ul>
+                                    <li><Image className="amazon logo" src={require('./images/tools/Amazon_Web_Services_Logo.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/CSS3_logo_and_wordmark.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/django-logo-negative.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/docker-logo.png')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/GitHub_Logo_White.png')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/heroku-logotype-vertical-white.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/HTML5_Logo.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/jQuery_White.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/PostgreSQL_logo.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/python-logo-generic.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/React-logo.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/riot.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/Sass Logo(Color).svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/semantic-ui.svg')}/></li>
+                                    <li><Image className="logo" src={require('./images/tools/Unofficial_JavaScript_logo_2.svg')}/></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="clients">
+                            <div className="primary">Clients I've worked with</div>
+                        </div>
+                    </Segment>
+
+                </div>
             </div>
         );
     }
