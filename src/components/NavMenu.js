1b6d954277f27396ebced1548dcce0d82f3cad08
BrendanMyers88 <myersb88@gmail.com>
2019-01-31 10:50:42 -0800

    Removes un-used navmenu items for now, will re-add when content is added back to the page

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -40,109 +40,109 @@ export class NavMenu extends Component {
                         </Menu.Item>
                     </Link>
 
-                    <Menu.Item
-                        position='right'
-                        name='about'
-                        active={activeItem === 'about'}
-                        onClick={this.handleItemClick}
-                        className="nav-item"
-                    >
-                        About
-                    </Menu.Item>
+                    {/*<Menu.Item*/}
+                        {/*position='right'*/}
+                        {/*name='about'*/}
+                        {/*active={activeItem === 'about'}*/}
+                        {/*onClick={this.handleItemClick}*/}
+                        {/*className="nav-item"*/}
+                    {/*>*/}
+                        {/*About*/}
+                    {/*</Menu.Item>*/}
 
-                    <Menu.Item
-                        name='projects'
-                        active={activeItem === 'projects'}
-                        onClick={this.handleItemClick}
-                        className="nav-item"
-                    >
-                        Projects
-                    </Menu.Item>
+                    {/*<Menu.Item*/}
+                        {/*name='projects'*/}
+                        {/*active={activeItem === 'projects'}*/}
+                        {/*onClick={this.handleItemClick}*/}
+                        {/*className="nav-item"*/}
+                    {/*>*/}
+                        {/*Projects*/}
+                    {/*</Menu.Item>*/}
 
-                    <Link to="/blog">
-                        <Menu.Item
-                            name='blog'
-                            active={activeItem === 'blog'}
-                            onClick={this.scrollToTop}
-                            className="nav-item"
-                        >
-                            Blog
-                        </Menu.Item>
-                    </Link>
+                    {/*<Link to="/blog">*/}
+                        {/*<Menu.Item*/}
+                            {/*name='blog'*/}
+                            {/*active={activeItem === 'blog'}*/}
+                            {/*onClick={this.scrollToTop}*/}
+                            {/*className="nav-item"*/}
+                        {/*>*/}
+                            {/*Blog*/}
+                        {/*</Menu.Item>*/}
+                    {/*</Link>*/}
 
-                    <Menu.Item
-                        name='contact'
-                        active={activeItem === 'Contact'}
-                        onClick={this.handleItemClick}
-                        className="nav-item"
-                    >
-                        Contact
-                    </Menu.Item>
+                    {/*<Menu.Item*/}
+                        {/*name='contact'*/}
+                        {/*active={activeItem === 'Contact'}*/}
+                        {/*onClick={this.handleItemClick}*/}
+                        {/*className="nav-item"*/}
+                    {/*>*/}
+                        {/*Contact*/}
+                    {/*</Menu.Item>*/}
 
-                    <Menu.Item
-                        position='right'
-                        icon='content'
-                        active={activeItem === 'hamburger'}
-                        onClick={this.handleShowClick}
-                        className="hamburger-btn"
-                    >
-                    </Menu.Item>
+                    {/*<Menu.Item*/}
+                        {/*position='right'*/}
+                        {/*icon='content'*/}
+                        {/*active={activeItem === 'hamburger'}*/}
+                        {/*onClick={this.handleShowClick}*/}
+                        {/*className="hamburger-btn"*/}
+                    {/*>*/}
+                    {/*</Menu.Item>*/}
 
                 </Menu>
-                <Sidebar
-                    as={Menu}
-                    animation='overlay'
-                    inverted
-                    onHide={this.handleSidebarHide}
-                    vertical
-                    visible={visible}
-                    width='thin'
-                >
-                    <Link to="/">
-                        <Menu.Item
-                            as='a'
-                            name='about'
-                            active={activeItem === 'about'}
-                            onClick={this.scrollToTop}
-                        >
-                            About
-                        </Menu.Item>
-                    </Link>
+                {/*<Sidebar*/}
+                    {/*as={Menu}*/}
+                    {/*animation='overlay'*/}
+                    {/*inverted*/}
+                    {/*onHide={this.handleSidebarHide}*/}
+                    {/*vertical*/}
+                    {/*visible={visible}*/}
+                    {/*width='thin'*/}
+                {/*>*/}
+                    {/*<Link to="/">*/}
+                        {/*<Menu.Item*/}
+                            {/*as='a'*/}
+                            {/*name='about'*/}
+                            {/*active={activeItem === 'about'}*/}
+                            {/*onClick={this.scrollToTop}*/}
+                        {/*>*/}
+                            {/*About*/}
+                        {/*</Menu.Item>*/}
+                    {/*</Link>*/}
 
-                    <Link to="/">
-                        <Menu.Item
-                            as='a'
-                            name='projects'
-                            active={activeItem === 'projects'}
-                            onClick={this.scrollToTop}
-                        >
-                            Projects
-                        </Menu.Item>
-                    </Link>
+                    {/*<Link to="/">*/}
+                        {/*<Menu.Item*/}
+                            {/*as='a'*/}
+                            {/*name='projects'*/}
+                            {/*active={activeItem === 'projects'}*/}
+                            {/*onClick={this.scrollToTop}*/}
+                        {/*>*/}
+                            {/*Projects*/}
+                        {/*</Menu.Item>*/}
+                    {/*</Link>*/}
 
-                    <Link to="/blog">
-                        <Menu.Item
-                            as='a'
-                            name='blog'
-                            active={activeItem === 'blog'}
-                            onClick={this.scrollToTop}
-                        >
-                            Blog
-                        </Menu.Item>
-                    </Link>
+                    {/*<Link to="/blog">*/}
+                        {/*<Menu.Item*/}
+                            {/*as='a'*/}
+                            {/*name='blog'*/}
+                            {/*active={activeItem === 'blog'}*/}
+                            {/*onClick={this.scrollToTop}*/}
+                        {/*>*/}
+                            {/*Blog*/}
+                        {/*</Menu.Item>*/}
+                    {/*</Link>*/}
 
-                    <Link to="/">
-                        <Menu.Item
-                            as='a'
-                            name='contact'
-                            active={activeItem === 'contact'}
-                            onClick={this.scrollToTop}
+                    {/*<Link to="/">*/}
+                        {/*<Menu.Item*/}
+                            {/*as='a'*/}
+                            {/*name='contact'*/}
+                            {/*active={activeItem === 'contact'}*/}
+                            {/*onClick={this.scrollToTop}*/}
 
-                        >
-                            Contact
-                        </Menu.Item>
-                    </Link>
-                </Sidebar>
+                        {/*>*/}
+                            {/*Contact*/}
+                        {/*</Menu.Item>*/}
+                    {/*</Link>*/}
+                {/*</Sidebar>*/}
             </div>
         );
     }
