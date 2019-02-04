298402be563a852a48301f57432064ce340ee04c
BrendanMyers88 <myersb88@gmail.com>
2019-02-04 14:59:06 -0800

    Updates

---
+++ precache-manifest.6d043dcf218c0e1df37da519eec781fd.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aaa82921843bfa937b8",
+    "url": "/static/css/main.b2d662b7.chunk.css"
+  },
+  {
+    "revision": "2aaa82921843bfa937b8",
+    "url": "/static/js/main.2aaa8292.chunk.js"
+  },
+  {
+    "revision": "7657b4e4c65e8e5036fb",
+    "url": "/static/js/1.7657b4e4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86e6e797a4c6ef13529108066c64647a",
+    "url": "/static/media/hero_bg2.86e6e797.jpeg"
+  },
+  {
+    "revision": "259e1339a21cf9a16881f722296cb704",
+    "url": "/static/media/Amazon_Web_Services_Logo.259e1339.svg"
+  },
+  {
+    "revision": "7415a70abb0fbaeab688508b2d8b1ce4",
+    "url": "/static/media/CSS3_logo_and_wordmark.7415a70a.svg"
+  },
+  {
+    "revision": "20c8e0ac9a15bb7d542ea1c8e8cb447c",
+    "url": "/static/media/django-logo-negative.20c8e0ac.svg"
+  },
+  {
+    "revision": "9ff0fb894fc6b4f5e3234ca3a101fe88",
+    "url": "/static/media/docker-logo.9ff0fb89.png"
+  },
+  {
+    "revision": "064ace4777ac86d7627f455f36975452",
+    "url": "/static/media/GitHub_Logo_White.064ace47.png"
+  },
+  {
+    "revision": "a35ca1e667faa396a72d832a586de13c",
+    "url": "/static/media/heroku-logotype-vertical-white.a35ca1e6.svg"
+  },
+  {
+    "revision": "a3492ddba4b979effdc4c62f5adf1bb2",
+    "url": "/static/media/HTML5_Logo.a3492ddb.svg"
+  },
+  {
+    "revision": "371b4c8408455b1de278b97f9c76fb80",
+    "url": "/static/media/jQuery_White.371b4c84.svg"
+  },
+  {
+    "revision": "45e46bc025b8fd350c09492ce01d4549",
+    "url": "/static/media/PostgreSQL_logo.45e46bc0.svg"
+  },
+  {
+    "revision": "7d9fccaffda0db8340fe3e803fee9c83",
+    "url": "/static/media/python-logo-generic.7d9fccaf.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/React-logo.05a3e30f.svg"
+  },
+  {
+    "revision": "205ca80becbf34290a2ad4a3492769b8",
+    "url": "/static/media/riot.205ca80b.svg"
+  },
+  {
+    "revision": "0c745b701b8a2665f88560bf1cff6b54",
+    "url": "/static/media/Sass Logo(Color).0c745b70.svg"
+  },
+  {
+    "revision": "23583c9d8deee23943b43041b81fc03f",
+    "url": "/static/media/Stylus-logo.23583c9d.svg"
+  },
+  {
+    "revision": "88388fc7dc855ba26e2d0ecdf4f07dc7",
+    "url": "/static/media/semantic-ui.88388fc7.svg"
+  },
+  {
+    "revision": "dbf60545a1888c33d76924cea4e2774a",
+    "url": "/static/media/wagtail-logo.dbf60545.svg"
+  },
+  {
+    "revision": "952f016a9663354dcce5277546e4ebcd",
+    "url": "/static/media/BrendanPhoto.952f016a.jpg"
+  },
+  {
+    "revision": "2126020c0d2138fa0d1c7c15bd74f396",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/brendanjmyers/precache-manifest.6fdb52f08697fe488b9514c7d372456d.js"
+  "/precache-manifest.6d043dcf218c0e1df37da519eec781fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/brendanjmyers/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
